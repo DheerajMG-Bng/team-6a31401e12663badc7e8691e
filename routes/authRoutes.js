2fef40f33e5c6c33d289bbf74c0625fd9ffd3e7c
Aashu Goswami <goswamiji505@gmail.com>
2026-06-04 18:07:55 +0530

    Updated Authentication middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 const {
-    registerUser,
-    loginUser
-} = require("../controllers/userController");
+    register,
+    login
+} = require('../middleware/authMiddleware');
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
